caaf4f88c42fc3f43f40e828ebcaa20eaef5443f
P-Vries <43247243+P-Vries@users.noreply.github.com>
2018-09-25 12:45:37 +0200

    Add files via upload

---
+++ Morse Vertaler/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Morse_Vertaler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbIO = new System.Windows.Forms.RichTextBox();
+            this.btnVanMorse = new System.Windows.Forms.Button();
+            this.btnNaarMorse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbIO
+            // 
+            this.rtbIO.Location = new System.Drawing.Point(12, 12);
+            this.rtbIO.Name = "rtbIO";
+            this.rtbIO.Size = new System.Drawing.Size(776, 397);
+            this.rtbIO.TabIndex = 0;
+            this.rtbIO.Text = "";
+            // 
+            // btnVanMorse
+            // 
+            this.btnVanMorse.Location = new System.Drawing.Point(687, 415);
+            this.btnVanMorse.Name = "btnVanMorse";
+            this.btnVanMorse.Size = new System.Drawing.Size(101, 23);
+            this.btnVanMorse.TabIndex = 1;
+            this.btnVanMorse.Text = "Vertaal van Morse";
+            this.btnVanMorse.UseVisualStyleBackColor = true;
+            this.btnVanMorse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnNaarMorse
+            // 
+            this.btnNaarMorse.Location = new System.Drawing.Point(12, 415);
+            this.btnNaarMorse.Name = "btnNaarMorse";
+            this.btnNaarMorse.Size = new System.Drawing.Size(104, 23);
+            this.btnNaarMorse.TabIndex = 2;
+            this.btnNaarMorse.Text = "Vertaal naar Morse";
+            this.btnNaarMorse.UseVisualStyleBackColor = true;
+            this.btnNaarMorse.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnNaarMorse);
+            this.Controls.Add(this.btnVanMorse);
+            this.Controls.Add(this.rtbIO);
+            this.Name = "Form1";
+            this.Text = "Morse vertaler";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbIO;
+        private System.Windows.Forms.Button btnVanMorse;
+        private System.Windows.Forms.Button btnNaarMorse;
+    }
+}
+
---
+++ Morse Vertaler/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Morse_Vertaler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //
+        //Methodes
+        //
+        private string TConvert(string tekst)//Methode die tekst omzet naar morse code.
+        {
+            string Out = "";
+            tekst = tekst.ToUpper();
+            foreach (char c in tekst) 
+            {
+                if (c == 'A') { Out = Out + @"._,"; }
+                if (c == 'B') { Out = Out + @"_...,"; }
+                if (c == 'C') { Out = Out + @"_._.,"; }
+                if (c == 'D') { Out = Out + @"_..,"; }
+                if (c == 'E') { Out = Out + @".,"; }
+                if (c == 'F') { Out = Out + @".._.,"; }
+                if (c == 'G') { Out = Out + @"__.,"; }
+                if (c == 'H') { Out = Out + @"....,"; }
+                if (c == 'I') { Out = Out + @"..,"; }
+                if (c == 'J') { Out = Out + @".___,"; }
+                if (c == 'K') { Out = Out + @"_._,"; }
+                if (c == 'L') { Out = Out + @"._..,"; }
+                if (c == 'M') { Out = Out + @"__,"; }
+                if (c == 'N') { Out = Out + @"_.,"; }
+                if (c == 'O') { Out = Out + @"___,"; }
+                if (c == 'P') { Out = Out + @".__.,"; }
+                if (c == 'Q') { Out = Out + @"__._,"; }
+                if (c == 'R') { Out = Out + @"._.,"; }
+                if (c == 'S') { Out = Out + @"...,"; }
+                if (c == 'T') { Out = Out + @"_,"; }
+                if (c == 'U') { Out = Out + @".._,"; }
+                if (c == 'V') { Out = Out + @"..._,"; }
+                if (c == 'W') { Out = Out + @".__,"; }
+                if (c == 'X') { Out = Out + @"_.._,"; }
+                if (c == 'Y') { Out = Out + @"_.__,"; }
+                if (c == 'Z') { Out = Out + @"__..,"; }
+                if (c == '1') { Out = Out + @".____,"; }
+                if (c == '2') { Out = Out + @"..___,"; }
+                if (c == '3') { Out = Out + @"...__,"; }
+                if (c == '4') { Out = Out + @"...._,"; }
+                if (c == '5') { Out = Out + @".....,"; }
+                if (c == '6') { Out = Out + @"_....,"; }
+                if (c == '7') { Out = Out + @"__...,"; }
+                if (c == '8') { Out = Out + @"___..,"; }
+                if (c == '9') { Out = Out + @"____.,"; }
+                if (c == '0') { Out = Out + @"_____,"; }
+                if (c == ' ') { Out = Out + @" ,"; }
+            }//We kijken per letter of cijfer welke lettere het is en voegen die toe aan de Out string die we net hebben gemaakt dit zou ook met een array kunnen maar ik probeer zo weinig mogelijk op te zoeken
+            return Out;
+        }
+
+        private string MConvert(string morse)
+        {
+            string Out = "";
+            string[] tekst = morse.Split(',');
+            foreach (string c in tekst)
+            {
+                if (c == @"._") { Out = Out + 'a'; }
+                if (c == @"_...") { Out = Out + 'b'; }
+                if (c == @"_._.") { Out = Out + 'c'; }
+                if (c == @"_..") { Out = Out + 'd'; }
+                if (c == @".") { Out = Out + 'e'; }
+                if (c == @".._.") { Out = Out + 'f'; }
+                if (c == @"__.") { Out = Out + 'g'; }
+                if (c == @"....") { Out = Out + 'h'; }
+                if (c == @"..") { Out = Out + 'i'; }
+                if (c == @".___") { Out = Out + 'j'; }
+                if (c == @"_._") { Out = Out + 'k'; }
+                if (c == @"._..") { Out = Out + 'l'; }
+                if (c == @"__") { Out = Out + 'm'; }
+                if (c == @"_.") { Out = Out + 'n'; }
+                if (c == @"___") { Out = Out + 'o'; }
+                if (c == @".__.") { Out = Out + 'p'; }
+                if (c == @"__._") { Out = Out + 'q'; }
+                if (c == @"._.") { Out = Out + 'r'; }
+                if (c == @"...") { Out = Out + 's'; }
+                if (c == @"_") { Out = Out + 't'; }
+                if (c == @".._") { Out = Out + 'u'; }
+                if (c == @"..._") { Out = Out + 'v'; }
+                if (c == @".__") { Out = Out + 'w'; }
+                if (c == @"_.._") { Out = Out + 'x'; }
+                if (c == @"_.__") { Out = Out + 'y'; }
+                if (c == @"__..") { Out = Out + 'z'; }
+                if (c == @".____") { Out = Out + '1'; }
+                if (c == @"..___") { Out = Out + '2'; }
+                if (c == @"...__") { Out = Out + '3'; }
+                if (c == @"...._") { Out = Out + '4'; }
+                if (c == @".....") { Out = Out + '5'; }
+                if (c == @"_....") { Out = Out + '6'; }
+                if (c == @"__...") { Out = Out + '7'; }
+                if (c == @"___..") { Out = Out + '8'; }
+                if (c == @"____.") { Out = Out + '9'; }
+                if (c == @"_____") { Out = Out + '0'; }
+                if (c == " ") { Out = Out + ' '; }
+            }//We kijken per code welke letter het is en voegen die toe aan de Out string die we net hebben gemaakt.
+            return Out;
+        }//Methode die Morse omzet naar Tekst
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rtbIO.Text = TConvert(rtbIO.Text);//Verwijzing naar de vertaal methode
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rtbIO.Text = MConvert(rtbIO.Text);//Verwijzing naar de vertaal methode
+        }
+    }
+}
